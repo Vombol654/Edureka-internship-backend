11f5e7f7c861e3e8da1024f7c397f51622003f4e
Vombol654 <97107431+Vombol654@users.noreply.github.com>
2022-01-05 08:38:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 const port=process.env.PORT||8989;
-const hostname="localhost";
+const hostname="0.0.0.0";
 const dburl = 'mongodb+srv://ananda_123:ananda_123@cluster0.1uqms.mongodb.net/zomato_clone?retryWrites=true&w=majority';
 
 app.use((req,res,next)=>{
